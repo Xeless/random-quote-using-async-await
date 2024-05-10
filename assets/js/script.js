1c18a0e958b6ff8680200227808cd94fa290302b
Xeless <tomdelintepro@gmail.com>
2024-05-10 16:43:26 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,72 @@
+const button = document.getElementById("test")
+
+
+
+button.addEventListener("click", async function(){
+
+
+
+    try {
+        const reponse = await fetch("https://thatsthespir.it/api")
+        console.log("test", reponse)
+
+        
+        const data = await reponse.json()
+        console.log("donne JSon", data)
+
+
+
+            const section = document.createElement("section")
+            const figure = document.createElement("figure")
+            const blockquote = document.createElement("blockquote")
+            const p = document.createElement("p")
+            p.textContent = data.quote
+
+            const figcaption = document.createElement("figcaption")
+            const div = document.createElement("div")
+            const img = document.createElement("img")
+            img.src = data.photo
+            const address = document.createElement("address")
+            address.textContent = "-"
+            const a = document.createElement("a")
+            a.textContent = data.author
+            const small = document.createElement("small")
+            small.textContent = data.total_quotes
+
+
+
+
+
+
+
+            document.body.appendChild(section)
+            section.appendChild(figure)
+            figure.appendChild(blockquote)
+            blockquote.appendChild(p)
+            section.appendChild(figcaption)
+            figcaption.appendChild(div)
+            div.appendChild(img)
+            div.appendChild(address)
+            address.appendChild(a)
+            a.appendChild(small)
+
+            
+        
+
+
+
+    }catch (error) {
+
+    
+    }
+
+
+
+})
+
+
+
+
+
+
+
